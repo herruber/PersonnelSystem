9e7d0648b36b7de74b58bde24502d1936ee9e86b
Robert <robertnyquist.rn@gmail.com>
2017-12-06 14:55:07 +0100

    Init

---
+++ ProjectPersonelSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjectPersonelSystem.Startup))]
+namespace ProjectPersonelSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
